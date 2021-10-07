979a5c8ccf7a18d3e5c2b9daedc7b8776aac8f8e
Mario Macias <mmaciasl@redhat.com>
2021-10-07 10:20:47 +0200

    NETOBSERV-41: unit tests on loki exporter
    To make the code testable, it has been refactored to allow dependency injection.
    
    In addition, linters have been added and other parts of the code have been
    refactored to pass the linters.
    
    Finally, the module dependencies have been vendored.

--- Makefile
+++ Makefile
@@ -7,9 +7,12 @@ CONTAINER_COMMAND ?= docker
 prereqs:
 	@echo "### Test if prerequisites are met, and installing missing dependencies"
 	test -f $(go env GOPATH)/bin/golangci-lint || go install github.com/golangci/golangci-lint/cmd/golangci-lint@${GOLANGCI_LINT_VERSION}
+	test -f $(go env GOPATH)/bin/staticcheck || go install honnef.co/go/tools/cmd/staticcheck@latest
 
 lint: prereqs
 	@echo "### Linting code"
+	# staticcheck does not work properly when invoked inside golangci-lint
+	staticcheck -f stylish ./...
 	golangci-lint run ./...
 
 image:
@@ -17,7 +20,7 @@ image:
 	${CONTAINER_COMMAND} build --build-arg VERSION=${ARTIFACT_VERSION} -t quay.io/jotak/goflow2:loki-latest .
 
 test:
-	echo "### Testing"
+	@echo "### Testing"
 	go test ./... -coverprofile ${COVERPROFILE}
 
 verify: lint test
--- cmd/loki-exporter.go
+++ cmd/loki-exporter.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
-	"strings"
-	"time"
 
-	logadapter "github.com/go-kit/kit/log/logrus"
-	"github.com/grafana/loki-client-go/loki"
-	jsoniter "github.com/json-iterator/go"
-	"github.com/netobserv/goflow2-loki-exporter/pkg/config"
-	"github.com/prometheus/common/model"
+	"github.com/netobserv/goflow2-loki-exporter/pkg/export"
 	"github.com/sirupsen/logrus"
 )
 
@@ -26,13 +18,6 @@ var (
 	log         = logrus.WithField("module", app)
 	appVersion  = fmt.Sprintf("%s %s", app, version)
 )
-
-var (
-	keyReplacer = strings.NewReplacer("/", "_", ".", "_", "-", "_")
-)
-
-func init() {
-}
 
 func main() {
 	flag.Parse()
@@ -50,108 +35,23 @@ func main() {
 	}
 	logrus.SetLevel(lvl)
 
-	var conf config.Config
-	if *configFile != "" {
-		conf, err = config.Load(*configFile)
-		if err != nil {
-			log.Fatal(err)
-		}
+	var config *export.Config
+	if configFile == nil || *configFile == "" {
+		log.Info("Using default configuration")
+		config = export.DefaultConfig()
 	} else {
-		log.Infof("Config file not provided, using defaults")
-		conf = config.Default()
-	}
-
-	log.Infof("Starting %s at log level %s", appVersion, *logLevel)
-
-	clientConfig, err := conf.BuildClientConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-	lokiClient, err := loki.NewWithLogger(clientConfig, logadapter.NewLogrusLogger(log))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		in := scanner.Bytes()
-		if conf.PrintInput {
-			fmt.Println(string(in))
-		}
-		err := processRecord(in, conf, lokiClient)
-		if err != nil {
-			log.Error(err)
+		flog := log.WithField("configFile", *configFile)
+		flog.Info("Provided YAML config file")
+		if config, err = export.LoadConfig(*configFile); err != nil {
+			flog.WithError(err).Fatal("Can't load config file")
 		}
 	}
-
-	if err = scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func processRecord(rawRecord []byte, conf config.Config, lokiClient *loki.Client) error {
-	// TODO: allow protobuf input
-	var record map[model.LabelName]interface{}
-	err := json.Unmarshal(rawRecord, &record)
+	loki, err := export.NewLoki(config)
 	if err != nil {
-		return err
+		log.WithError(err).Fatal("Can't load Loki exporter")
 	}
-
-	// Get timestamp from record (default: TimeFlowStart)
-	timestamp := time.Now()
-	if conf.TimestampLabel != "" {
-		if t, ok := record[conf.TimestampLabel]; ok {
-			if ft, ok := t.(float64); ok {
-				unix := int64(ft * conf.TimestampScaleToSecond)
-				if unix != 0 {
-					timestamp = time.Unix(unix, 0)
-				} else {
-					log.Warnf("Empty timestamp (%s) found in record, use now instead", conf.TimestampLabel)
-				}
-			} else {
-				log.Warnf("Invalid timestamp (%s: %v) found in record: number explected", conf.TimestampLabel, t)
-			}
-		} else {
-			log.Warnf("Timestamp label %s not found in record", conf.TimestampLabel)
-		}
+	if err := loki.Process(os.Stdin); err != nil {
+		log.WithError(err).Fatal("Processing standard input")
 	}
-
-	labels := model.LabelSet{}
-
-	// Add static labels from config
-	for k, v := range conf.StaticLabels {
-		labels[k] = v
-	}
-
-	// Add non-static labels from record
-	for _, label := range conf.Labels {
-		if val, ok := record[label]; ok {
-			sanitizedKey := model.LabelName(keyReplacer.Replace(string(label)))
-			if sanitizedKey.IsValid() {
-				lv := model.LabelValue(fmt.Sprintf("%v", val))
-				if lv.IsValid() {
-					labels[sanitizedKey] = lv
-				} else {
-					log.Infof("Invalid value: %v", lv)
-				}
-			} else {
-				log.Infof("Invalid label: %v", sanitizedKey)
-			}
-		}
-	}
-
-	// Remove labels and configured ignore list from record
-	ignoreList := append(conf.IgnoreList, conf.Labels...)
-	for _, label := range ignoreList {
-		delete(record, label)
-	}
-
-	js, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(record)
-	if err != nil {
-		return err
-	}
-	if conf.PrintOutput {
-		fmt.Println(string(js))
-	}
-	return lokiClient.Handle(labels, timestamp, string(js))
+	log.Warn("Process finished")
 }
